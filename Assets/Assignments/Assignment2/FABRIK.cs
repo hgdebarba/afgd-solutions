bd175b1b49706ce68423c6d7eea7df644e996f67
henrique <hgdebarba@gmail.com>
2021-03-08 13:12:47 +0100

    assignment 2 and examples 6

---
+++ Assets/Assignments/Assignment2/FABRIK.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AfGD.Assignment2
+{
+    public class FABRIK : MonoBehaviour
+    {
+        [Tooltip("the joints that we are controlling")]
+        public Transform[] joints;
+        [Tooltip("target that our end effector is trying to reach")]
+        public Transform target;
+
+        [Tooltip("error tolerance, will stop updating after distance between end effector and target is smaller than tolerance.")]
+        [Range(.01f, .2f)]
+        public float tolerance = 0.05f;
+
+        [Tooltip("maximum number of iterations before we follow to the next frame")]
+        [Range(1, 100)]
+        public int maxIterations = 20;
+
+        [Tooltip("rotation constraint. " +
+        	"Instead of an elipse with 4 rotation limits, " +
+        	"we use a circle with a single rotation limit. " +
+        	"Implementation will be simpler than in the paper.")]
+        [Range(0f, 180f)]
+        public float rotationLimit = 45f;
+
+        // distances/lengths between joints.
+        private float[] distances;
+        // total length of the system
+        private float chainLength;
+
+
+        Vector3 EnforceConstraints(Vector3 P, Vector3 Pprev, Vector3 Pnext)
+        {
+            Vector3 Ptgt = Pnext;
+
+            Vector3 l = P - Pprev;
+            Vector3 lnext = Pnext - P;
+
+            if (Vector3.Angle(l, lnext) < rotationLimit)
+                return Ptgt;
+
+
+            Vector3 o = Vector3.Project(lnext, l);
+
+            if (Vector3.Dot(o, l) < 0 && rotationLimit <= 90)
+            {
+                o = -o;
+                lnext = Vector3.Reflect(lnext, o);
+            }
+
+            Vector3 Po = P + o;
+            float r = Mathf.Abs((float)((double)o.magnitude * (double)Mathf.Tan(rotationLimit * Mathf.Deg2Rad)));
+
+            Debug.DrawLine(P, P + o, Color.red);
+            Debug.DrawLine(P, P + lnext, Color.blue);
+
+            Vector3 d = P + lnext - Po;
+            Ptgt = Po + r * d.normalized;
+
+            return Ptgt;
+        }
+
+        private void Solve()
+        {
+            // YOUR IMPLEMENTATION HERE
+            // FEEL FREE TO CREATE HELPER FUNCTIONS
+
+            float dist = (joints[0].position - target.position).magnitude;
+            // out of reach
+            if (dist > chainLength)
+            {
+                for (int i = 0; i < joints.Length - 1; i++)
+                {
+                    float ri = (joints[i].position - target.position).magnitude;
+                    float lambdai = distances[i] / ri;
+                    joints[i + 1].position = (1.0f - lambdai) * joints[i].position + lambdai * target.position;
+
+                    Vector3 right = (joints[i + 1].position - joints[i].position).normalized;
+                    Vector3 forward = Vector3.Cross(right, Vector3.up).normalized;
+                    Vector3 up = Vector3.Cross(forward, right).normalized;
+                    Matrix4x4 rotation = new Matrix4x4(right, up, forward, new Vector4(0, 0, 0, 1));
+                    joints[i].rotation = rotation.rotation;
+                    
+                }
+                joints[joints.Length - 1].rotation = target.rotation;
+            }
+            else
+            {
+                Vector3 b = joints[0].position;
+
+                for (int iterations = 0; iterations < maxIterations; iterations++)
+                {
+                    joints[joints.Length - 1].position = target.position;
+
+                    // forward reaching
+                    for (int i = joints.Length - 2; i >= 0; i--)
+                    {
+                        Vector3 limitTarget = joints[i].position;
+                        if (i != joints.Length - 2) // we skip the last joint
+                            limitTarget = EnforceConstraints(joints[i + 1].position, joints[i + 2].position, joints[i].position);
+
+                        float ri = (joints[i + 1].position - limitTarget).magnitude;
+                        float lambdai = distances[i] / ri;
+                        joints[i].position = (1 - lambdai) * joints[i + 1].position + lambdai * limitTarget;
+
+                    }
+
+                    // backward reaching
+                    joints[0].position = b;
+                    for (int i = 0; i < joints.Length - 1; i++)
+                    {
+                        Vector3 limitTarget = joints[i + 1].position;
+                        if (i != 0) // we skip the root joint
+                            limitTarget = EnforceConstraints(joints[i].position, joints[i - 1].position, joints[i + 1].position);
+
+                        float ri = (joints[i].position - limitTarget).magnitude;
+                        float lambdai = distances[i] / ri;
+                        joints[i + 1].position = (1 - lambdai) * joints[i].position + lambdai * limitTarget;
+                    }
+                }
+
+                // set rotation of the joints
+                joints[joints.Length - 1].rotation = target.rotation;
+                for (int i = 0; i < joints.Length - 1; i++)
+                {
+                    // build a rotation matrix by defining three orthonormal axis
+                    Vector3 right = (joints[i + 1].position - joints[i].position).normalized;
+                    Vector3 forward = Vector3.Cross(right, Vector3.up).normalized;
+                    Vector3 up = Vector3.Cross(forward, right).normalized;
+                    Matrix4x4 rotation = new Matrix4x4(right, up, forward, new Vector4(0, 0, 0, 1));
+                    joints[i].rotation = rotation.rotation;
+                    // has the same effect as joints[i].right = (joints[i + 1].position - joints[i].position).normalized;
+                }
+
+            }
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // pre-compute segment lenghts and total length of the chain
+            // we assume that the segment length (aka bone length) is constant
+            distances = new float[joints.Length-1];
+            chainLength = 0;
+            // If we have N joints, then there are N-1 segment/bone lengths
+            for (int i = 0; i < joints.Length - 1; i++)
+            {
+                distances[i] = (joints[i + 1].position - joints[i].position).magnitude;
+                chainLength += distances[i];
+            }
+        }
+
+
+        void Update()
+        {
+            Solve();
+            for (int i = 1; i < joints.Length - 1; i++)
+            {
+                DebugJointLimit(joints[i], joints[i - 1], rotationLimit, 2);
+            }
+        }
+
+        void DebugJointLimit(Transform tr, Transform trPrev, float angle, float scale = 1)
+        {
+            float angleRad = Mathf.Deg2Rad * angle;
+            float dist = Mathf.Sin(Mathf.Deg2Rad * angle);
+            int steps = 72;
+            float stepSize = 360f / steps;
+            // 36 is the number of line segments used to draw the cone
+            for (int i = 0; i < steps; i++)
+            {
+                Vector3 vec = new Vector3(1, 0, 0);
+                float twistRad = Mathf.Deg2Rad * i * stepSize;
+                vec.x = Mathf.Cos(angleRad);
+                vec.y = Mathf.Cos(twistRad) * dist;
+                vec.z = Mathf.Sin(twistRad) * dist;
+                vec = trPrev.rotation * vec;// * tr.localToWorldMatrix.MultiplyVector(vec);
+                Debug.DrawLine(tr.position, tr.position + vec * scale, Color.white);
+            }
+
+        }
+    }
+
+}
---
+++ Assets/Exercises/Examples6/TrigonometricIK.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AfGD.Examples6
+{
+    // this implementation only works in the x/y plane
+    // it assumes that a segment with angle 0 points to +x-axis, and +90 points to +y-axis
+    public class TrigonometricIK : MonoBehaviour
+    {
+        [Tooltip("the joints that we are controlling")]
+        public Transform topJoint, midJoint, endJoint;
+        [Tooltip("target that our endJoint is trying to reach")]
+        public Transform target;
+        [Tooltip("there are two possible solutions to the 3 joint IK problem on the plane")]
+        public bool alternateSolution = false;
+
+        // rotation angle to be used for rotating top and middle joints
+        private float topAngle = 0, midAngle = 0;
+
+
+        public void Solve()
+        {
+            // CLASS CODE HERE
+            Vector3 t2tgt = target.position - topJoint.position;  // these are the lengths of the edges of our triangle
+            float a = (midJoint.position - topJoint.position).magnitude;
+            float b = (endJoint.position - midJoint.position).magnitude;float c = t2tgt.magnitude;
+            float angleToTgt = Vector3.Angle(Vector3.right, t2tgt.normalized); 
+            // we need to make a distinction between clockwise and counter-cw
+            if (t2tgt.y < 0) angleToTgt *= -1;
+
+            if (a + b < c)
+            { // not a valid triangle! 
+                // kinematic chain is fully extended 
+                topAngle = angleToTgt;
+                midAngle = 0;
+            }
+            else
+            {
+                // law of cosines to solve for the angles 
+                float gamma = Mathf.Acos((-c * c + a * a + b * b) / (2 * a * b));
+                gamma *= Mathf.Rad2Deg * (alternateSolution ? -1 : 1);
+
+                float beta = Mathf.Acos((-b * b + a * a + c * c) / (2 * a * c));
+                beta *= Mathf.Rad2Deg * (alternateSolution ? -1 : 1);
+                // transform from angles in a triangle to angles in our plane
+                topAngle = angleToTgt + beta;
+                midAngle = 180 + gamma;
+            }
+
+            // we control rotation, notice that we assume all rotations are around the z axis
+            topJoint.localRotation = Quaternion.AngleAxis(topAngle, Vector3.forward);
+            midJoint.localRotation = Quaternion.AngleAxis(midAngle, Vector3.forward);
+        }
+
+
+        void Update()
+        {
+            Solve();
+
+        }
+    }
+
+}
